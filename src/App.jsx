a50a6d14e3e8f9aa520dcaadbaf72aadf91306f0
ZIEDSAGGUEM <139626311+ZIEDSAGGUEM@users.noreply.github.com>
2023-07-15 12:37:24 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './App.css'
+
+
+const API_KEY = "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";
+
+
+
+function App() {
+  const [Tweet, setTweet] = useState("")
+  const [sentiment,setSentiment] = useState("")
+
+  async function CallOpenAiAPI(){
+    console.log("OpenAIAPI Called!")
+
+
+    const APIBODY ={
+      "model": "text-davinci-003",
+      "prompt": "What's the sentiment of this Tweet ? "+  Tweet,
+      "temperature": 0,
+      "max_tokens": 60,
+      "top_p": 1.0,
+      "frequency_penalty": 0.0,
+      "presence_penalty": 0.0
+    }
+
+
+
+    await fetch("https://api.openai.com/v1/completions",{
+      method:"POST",
+      headers:{
+      "Content-Type": "application/json" ,
+      "Authorization": "Bearer "+ API_KEY
+      },
+      body:JSON.stringify(APIBODY)
+
+    }).then((data) => {
+    return data.json()
+    }).then((data)=>{
+        console.log(data);
+        setSentiment(data.choices[0].text.trim());
+  })  
+  }
+  console.log(Tweet);
+
+  return (
+    <div className="App">
+      <div>
+      <textarea
+      onChange={(e) =>  setTweet(e.target.value)}
+      placeholder='Paste your tweet here!'
+      cols={50}
+      rows={10}
+      
+      />
+      </div>
+
+      <div>
+        <button onClick={CallOpenAiAPI}>Get The Tweet Sentiment From OpenAI API</button>
+        {sentiment !== "" ?
+        <h3>This Tweet is : {sentiment}</h3>:
+         null
+      
+      }
+      </div>
+        
+      
+    </div>
+  )
+}
+
+export default App
